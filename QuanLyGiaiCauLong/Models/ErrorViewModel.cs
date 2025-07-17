95f4cf2460a4bebf0fc519f3bf1241a2d399d0d5
Luu Hung <hungluu344@gmail.com>
2025-07-17 21:00:11 +0700

    Add project files.

---
+++ QuanLyGiaiCauLong/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyGiaiCauLong.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
